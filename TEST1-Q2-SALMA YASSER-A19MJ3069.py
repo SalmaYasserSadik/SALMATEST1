f8be727768a274c9c6508609ec0064d26f8f05a8
SalmaYasserSadik <sadik.salma@graduate.utm.my>
2023-11-15 15:39:08 +0800

    commit

--- TEST1-Q2-SALMA YASSER-A19MJ3069.py
+++ TEST1-Q2-SALMA YASSER-A19MJ3069.py
@@ -1,12 +1,17 @@
-def analyze_string(input_string):
-    total_chars = len(input_string)
+#Name: SALMA YASSER SADIK  Matric NO.: A19MJ3069
+#  QUESTION 2
+# You are tasked with implementing a simple program to analyze a given string. Write a Python 
+# program that performs the following tasks:
+def analyze_string(input_str): #Takes a string as input from the user.
+    """Analyze the given string"""
+    total_characters = len(input_str) # this line is changed if the total_characters = 0, it can't analyze the string from user. 
     uppercase_count = 0
     lowercase_count = 0
     digit_count = 0
-    is_palindrome = False
+    is_palindrome = False # intialize as False 
 
     # Analyze the string
-    for char in input_string:
+    for char in input_str:
         if char.isupper():
             uppercase_count += 1
         elif char.islower():
@@ -15,19 +20,20 @@ def analyze_string(input_string):
             digit_count += 1
 
     # Check if the string is a palindrome
-    reversed_string = input_string[::-1]
-    if input_string == reversed_string:
+    reversed_string = input_str[::-1]
+    if input_str == reversed_string:
         is_palindrome = True
 
-    # Print the results
-    print("Total number of characters:", total_chars)
-    print("Number of uppercase letters:", uppercase_count)
-    print("Number of lowercase letters:", lowercase_count)
-    print("Number of digits:", digit_count)
-    print("Is the string a palindrome:", is_palindrome)
+    # Print the results # i have removed the def main function
+    print("Total characters:", total_characters)  # The total number of characters in the string
+    print("Uppercase letters:", uppercase_count) #The number of uppercase letters in the string
+    print("Lowercase letters:", lowercase_count) #The number of lowercase letters in the string
+    print("Digits:", digit_count) # The number of digits in the string
+    print("Is the string a palindrome:", is_palindrome) #Whether the string is a palindrome or not
 
 # Get the string from the user
-input_string = input("Enter a string: ")
+print(f"\nExample Input:")  # print the statement Example input
+input_str = input("Enter a string: ")
 
 # Analyze and print the results
-analyze_string(input_string)
+analyze_string(input_str)
